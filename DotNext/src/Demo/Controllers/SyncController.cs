2a5f68b94c38b7d123428748d6492152ed9afd25
Denis Ivanov <d.ivanov@2gis.ru>
2017-05-02 16:48:45 +0700

    basic demos

---
+++ DotNext/src/Demo/Controllers/SyncController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("[controller]")]
+    [Route("{api-version:apiVersion}/[controller]")]
+    public sealed class SyncController : Controller
+    {
+        private readonly RemoteService _remoteService;
+
+        public SyncController(RemoteService remoteService)
+        {
+            _remoteService = remoteService;
+        }
+
+        [HttpGet("1")]
+        public IEnumerable<string> Demo1()
+        {
+            var task = _remoteService.IOBoundOperationAsync(1);
+            var result = task.Result;
+
+            return result;
+        }
+
+        [HttpGet("2")]
+        public IEnumerable<string> Demo2()
+        {
+            var task1 = _remoteService.IOBoundOperationAsync(2);
+            var task2 = _remoteService.IOBoundOperationAsync(2);
+
+            var result1 = task1.Result;
+            var result2 = task2.Result;
+
+            return result1.Concat(result2);
+        }
+
+        [HttpGet("3")]
+        public IEnumerable<string> Demo3()
+        {
+            var task = Task.Run(
+                () =>
+                    {
+                        var task1 = _remoteService.IOBoundOperationAsync(2);
+                        var task2 = _remoteService.IOBoundOperationAsync(2);
+
+                        var result1 = task1.Result;
+                        var result2 = task2.Result;
+
+                        return result1.Concat(result2);
+                    });
+
+            return task.Result;
+        }
+    }
+}
---
+++ DotNext/src/Demo/RemoteService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+namespace Demo
+{
+    public sealed class RemoteService
+    {
+        private readonly ILogger<RemoteService> _logger;
+
+        public RemoteService(ILogger<RemoteService> logger)
+        {
+            _logger = logger;
+        }
+
+        // ReSharper disable once InconsistentNaming
+        public async Task<IEnumerable<string>> IOBoundOperationAsync(int sec)
+        {
+            _logger.LogInformation("Remote service call started. Delay is {delay} second(s)", sec);
+            await Task.Delay(sec * 1000);
+            _logger.LogInformation("Remote service call finished.", sec);
+
+            return new[] { Guid.NewGuid().ToString() };
+        }
+    }
+}
